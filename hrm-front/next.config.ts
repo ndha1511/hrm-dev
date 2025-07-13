2ee4120fe0b90c30c2a9debc89a095f9b816168f
Hoang Anh <114679553+ndha1511@users.noreply.github.com>
2025-07-13 17:06:14 +0700

    Revert "Auto deploy on dockerhub"

--- hrm-front/next.config.ts
+++ hrm-front/next.config.ts
@@ -6,10 +6,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
-  /**
-   * @see https://nextjs.org/docs/pages/api-reference/config/next-config-js/output
-   */
-  output: 'standalone'
 };
 
 export default nextConfig;

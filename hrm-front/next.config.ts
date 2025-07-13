f0a8ecef88e0e34f62fc215759d0786146e9afd1
hoanganh <ndha1115@gmail.com>
2025-07-13 16:53:32 +0700

    feat: nextjs standalone
    #25

--- hrm-front/next.config.ts
+++ hrm-front/next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
+  /**
+   * @see https://nextjs.org/docs/pages/api-reference/config/next-config-js/output
+   */
+  output: 'standalone'
 };
 
 export default nextConfig;
